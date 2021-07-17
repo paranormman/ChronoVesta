3cf417350fc5d36325852a6d415733c45d099d9c
Manoj <manojshivaprakash10@gmail.com>
2021-07-18 00:41:44 +0530

    update 0.0

--- watermark.py
+++ watermark.py
@@ -1,14 +1,25 @@
 import fitz
+import numpy as np
 
 
 def remove_img_on_pdf(idoc, page):
     img_list = idoc.getPageImageList(page)
+    b = [(13, 0, 120, 74, 8, 'DeviceRGB', '', 'Image1', 'DCTDecode')]
+    b = np.array(b)
+    for x in img_list:
+        for y in b:
+            if img_list[x] == b[y]:
+                img_removal = img_list[x]
+            else:
+                continue
+            print(img_removal)
+
     con_list = idoc[page].get_contents()
 
     for i in con_list:
         c = idoc.xref_stream(i)
         if c != None:
-            for v in img_list:
+            for v in img_removal:
                 arr = bytes(v[7], 'utf-8')
                 r = c.find(arr)
                 if r != -1:
